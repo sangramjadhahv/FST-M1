bf2cc430358f2a4c4b19c2249c4607490f269479
sangramjadhahv <jadhavsangram2@gmail.com>
2024-09-04 12:29:08 +0530

    Update Activity3.java

--- Rest_Assured/Activities/Activity3.java
+++ Rest_Assured/Activities/Activity3.java
@@ -1,4 +1,4 @@
-package activities;
+package Activities;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -15,66 +15,83 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class Activity3 {
-
+   
     RequestSpecification requestSpec;
+   
     ResponseSpecification responseSpec;
 
     @BeforeClass
     public void setUp() {
+       
         requestSpec = new RequestSpecBuilder()
+               
                 .setContentType(ContentType.JSON)
+            
                 .setBaseUri("https://petstore.swagger.io/v2/pet")
+             
                 .build();
 
         responseSpec = new ResponseSpecBuilder()
+             
                 .expectStatusCode(200)
+            
                 .expectContentType("application/json")
+         
                 .expectBody("status", equalTo("alive"))
+           
                 .build();
     }
 
     @DataProvider
     public Object[][] petInfoProvider() {
-        Object[][] testData = new Object[][] {
-                { 77232, "Riley", "alive" },
-                { 77233, "Hansel", "alive" }
+      
+        Object[][] testData = new Object[][]{
+                {77232, "Riley", "alive"},
+                {77233, "Hansel", "alive"}
         };
         return testData;
     }
 
-    @Test(priority=1)
+    @Test(priority = 1)
+   
     public void addPets() {
         String reqBody = "{\"id\": 77232, \"name\": \"Riley\", \"status\": \"alive\"}";
-        Response response = given().spec(requestSpec)
-                .body(reqBody)
-                .when().post();
+        Response response = given().spec(requestSpec) 
+                .body(reqBody) 
+                .when().post(); 
 
         reqBody = "{\"id\": 77233, \"name\": \"Hansel\", \"status\": \"alive\"}";
-        response = given().spec(requestSpec)
-                .body(reqBody)
-                .when().post();
+        response = given().spec(requestSpec) 
+                .body(reqBody) 
+                .when().post(); 
 
-        response.then().spec(responseSpec);
+        
+        response.then().spec(responseSpec); 
     }
 
-    @Test(dataProvider = "petInfoProvider", priority=2)
+   
+    @Test(dataProvider = "petInfoProvider", priority = 2)
     public void getPets(int id, String name, String status) {
-        Response response = given().spec(requestSpec)
-                .pathParam("petId", id)
-                .when().get("/{petId}");
+        Response response = given().spec(requestSpec) 
+                .pathParam("petId", id) 
+                .when().get("/{petId}"); 
 
+      
+        System.out.println(response.asPrettyString());
+      
         response.then()
-                .spec(responseSpec)
-                .body("name", equalTo(name));
+                .spec(responseSpec) 
+                .body("name", equalTo(name)); 
     }
 
-    @Test(dataProvider = "petInfoProvider", priority=3)
+  
+    @Test(dataProvider = "petInfoProvider", priority = 3)
     public void deletePets(int id, String name, String status) {
-        Response response = given().spec(requestSpec)
-                .pathParam("petId", id)
-                .when().delete("/{petId}");
+        Response response = given().spec(requestSpec) 
+                .pathParam("petId", id) 
+                .when().delete("/{petId}"); 
 
-        // Assertions
+      
         response.then().body("code", equalTo(200));
     }
 
