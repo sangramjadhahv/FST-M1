c48410b48b5d917118bea617ab1e968df6483d3e
sangramjadhahv <jadhavsangram2@gmail.com>
2024-09-04 16:47:40 +0530

    Update Activity3.java

--- Rest_Assured/Activities/Activity3.java
+++ Rest_Assured/Activities/Activity3.java
@@ -1,5 +1,3 @@
-package Activities;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
@@ -15,83 +13,83 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class Activity3 {
-   
+    // Declare request specification
     RequestSpecification requestSpec;
-   
+    // Declare response specification
     ResponseSpecification responseSpec;
 
     @BeforeClass
     public void setUp() {
-       
+        // Create request specification
         requestSpec = new RequestSpecBuilder()
-               
+                // Set content type
                 .setContentType(ContentType.JSON)
-            
+                // Set base URL
                 .setBaseUri("https://petstore.swagger.io/v2/pet")
-             
+                // Build request specification
                 .build();
 
         responseSpec = new ResponseSpecBuilder()
-             
+                // Check status code in response
                 .expectStatusCode(200)
-            
+                // Check response content type
                 .expectContentType("application/json")
-         
+                // Check if response contains name property
                 .expectBody("status", equalTo("alive"))
-           
+                // Build response specification
                 .build();
     }
 
     @DataProvider
     public Object[][] petInfoProvider() {
-      
-        Object[][] testData = new Object[][]{
-                {45232, "Omkar", "alive"},
-                {77233, "Kriti", "alive"}
+        // Setting parameters to pass to test case
+        Object[][] testData = new Object[][] { 
+            { 77232, "Riley", "alive" }, 
+            { 77233, "Hansel", "alive" }
         };
         return testData;
     }
 
-    @Test(priority = 1)
-   
+    @Test(priority=1)
+    // Test case using a DataProvider
     public void addPets() {
         String reqBody = "{\"id\": 77232, \"name\": \"Riley\", \"status\": \"alive\"}";
-        Response response = given().spec(requestSpec) 
-                .body(reqBody) 
-                .when().post(); 
+        Response response = given().spec(requestSpec) // Use requestSpec
+                .body(reqBody) // Send request body
+                .when().post(); // Send POST request
 
         reqBody = "{\"id\": 77233, \"name\": \"Hansel\", \"status\": \"alive\"}";
-        response = given().spec(requestSpec) 
-                .body(reqBody) 
-                .when().post(); 
+        response = given().spec(requestSpec) // Use requestSpec
+                .body(reqBody) // Send request body
+                .when().post(); // Send POST request
 
-        
-        response.then().spec(responseSpec); 
+        // Assertions
+        response.then().spec(responseSpec); // Use responseSpec
     }
 
-   
-    @Test(dataProvider = "petInfoProvider", priority = 2)
+    // Test case using a DataProvider
+    @Test(dataProvider = "petInfoProvider", priority=2)
     public void getPets(int id, String name, String status) {
-        Response response = given().spec(requestSpec) 
-                .pathParam("petId", id) 
-                .when().get("/{petId}"); 
+        Response response = given().spec(requestSpec) // Use requestSpec
+                .pathParam("petId", id) // Add path parameter
+                .when().get("/{petId}"); // Send GET request
 
-      
+        // Print response
         System.out.println(response.asPrettyString());
-      
+        // Assertions
         response.then()
-                .spec(responseSpec) 
-                .body("name", equalTo(name)); 
+        .spec(responseSpec) // Use responseSpec
+        .body("name", equalTo(name)); // Additional Assertion
     }
 
-  
-    @Test(dataProvider = "petInfoProvider", priority = 3)
+    // Test case using a DataProvider
+    @Test(dataProvider = "petInfoProvider", priority=3)
     public void deletePets(int id, String name, String status) {
-        Response response = given().spec(requestSpec) 
-                .pathParam("petId", id) 
-                .when().delete("/{petId}"); 
+        Response response = given().spec(requestSpec) // Use requestSpec
+                .pathParam("petId", id) // Add path parameter
+                .when().delete("/{petId}"); // Send GET request
 
-      
+        // Assertions
         response.then().body("code", equalTo(200));
     }
 
