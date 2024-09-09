75c5877cb10f897a74b0667b8be22717ed0e34b4
Sangram Jadhav <sangram.jadhav1@ibm.com>
2024-09-09 16:02:35 +0530

    Git Appium Activity

--- Appium/Activities/Activity1.java
+++ Appium/Activities/Activity1.java
@@ -1,43 +1,39 @@
-package Activities;
+package activity;
 
-import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-
-
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class Activity1 {
-	  // Driver Declaration
-    AndroidDriver driver;
- 
-    // Set up method
-    @BeforeClass
-    public void setUp() throws MalformedURLException {
-        // Desired Capabilities
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setPlatformName("android");
-        options.setAutomationName("UiAutomator2");
-        options.setAppPackage("com.android.calculator2");
-        options.setAppActivity(".Calculator");
-        options.noReset();
- 
-        // Server Address
-        URL serverURL = new URL("http://localhost:4723/");
- 
-        // Driver Initialization
-        driver = new AndroidDriver(serverURL, options);
-    }
- 
-    // Test method
+
+	AndroidDriver driver;
+	WebDriverWait wait;
+	@BeforeClass
+	public void setup() throws MalformedURLException {
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setPlatformName("android");
+		options.setAutomationName("UiAutomator2");
+		options.setAppPackage("com.google.android.calculator");
+		options.setAppActivity("com.android.calculator2.Calculator");
+		options.noReset();
+		
+		URL serverUrl = new URL("http://localhost:4723/");
+		
+		driver = new AndroidDriver(serverUrl, options);
+	} 
+	// Test method
     @Test
     public void multiplyTest() {
         // Perform the calculation
@@ -47,12 +43,44 @@ public class Activity1 {
         driver.findElement(AppiumBy.accessibilityId("equals")).click();
  
         // Find the result
-        String result = driver.findElement(AppiumBy.id("result")).getText();
+        String result = driver.findElement(AppiumBy.id("result_final")).getText();
  
         // Assertion
         Assert.assertEquals(result, "40");
     }
  
+    @Test
+    public void multiplyTestLocators() {
+        // Perform the calculation
+     
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[contains(@resource-id, 'digit5') AND (@text = '5')]")).click();
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        driver.findElement(AppiumBy.id("digit_8")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+ 
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result_final")).getText();
+ 
+        // Assertion
+        Assert.assertEquals(result, "40");
+    }
+    
+    @Test
+    public void listElements() {
+        // Wait for elements to load
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+            AppiumBy.className("android.webkit.WebView")
+        ));
+ 
+        // Find all text elements on the page
+        List<WebElement> textItems = driver.findElements(
+            AppiumBy.xpath("//android.view.View/android.widget.Button")
+        );
+        System.out.println(textItems.size());
+        for (WebElement textItem : textItems) {
+            System.out.println(textItem.getText());
+        }
+    }
  
     // Tear down method
     @AfterClass
@@ -60,4 +88,4 @@ public class Activity1 {
         // Close the app
         driver.quit();
     }
-    }
+}
